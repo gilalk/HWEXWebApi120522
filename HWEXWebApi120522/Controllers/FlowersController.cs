25e7b89ccfeb2d673155f4156c84a91b39c1a8be
Gil Alkobi <86377486+gilalk@users.noreply.github.com>
2022-05-20 04:03:58 +0300

    final commit

---
+++ HWEXWebApi120522/Controllers/FlowersController.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HWEXWebApi120522.Controllers
+{
+    [ApiController]
+    [Route("test/[action]")]
+    public class FlowersController
+    {
+        public string Calanit()
+        {
+            return "Hello from Calanit Method";
+        }
+        public string Rakefet()
+        {
+            return "Hello from Rakefet Method";
+        }
+        public string Narkis()
+        {
+            return "Hello from Narkis Method";
+        }
+        public string Shoshan()
+        {
+            return "Hello from Shoshan Method";
+        }
+    }
+}
